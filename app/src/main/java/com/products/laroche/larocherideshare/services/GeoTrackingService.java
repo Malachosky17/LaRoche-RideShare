dcc8b81b86f4ee05a3ef20ddda11bfc3807dcd43
Joseph Malachosky <Joseph.Malachosky@stu.laroche.edu>
2017-05-03 14:12:43 -0400

    Final project committing...

---
+++ app/src/main/java/com/products/laroche/larocherideshare/services/GeoTrackingService.java
@@ -0,0 +1,16 @@
+package com.products.laroche.larocherideshare.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class GeoTrackingService extends Service {
+    public GeoTrackingService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
---
+++ app/src/main/java/com/products/laroche/larocherideshare/services/GeofencingTransitionIntentService.java
@@ -0,0 +1,12 @@
+package com.products.laroche.larocherideshare.services;
+
+import android.app.IntentService;
+
+/**
+ * Created by Malac on 2/23/2017.
+ *
+ * @author: Malac
+ */
+
+public class GeofencingTransitionIntentService extends IntentService {
+}
